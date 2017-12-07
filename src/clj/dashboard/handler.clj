951a41b0b73c4fbcbad476530669bf9a7712e9ca
kharper <keithharper1155@gmail.com>
2017-12-07 09:17:03 -0500

    Fixed some keywords that were accidentally refactored. Added response-as-json function to clean up some duplicated code.

--- src/clj/dashboard/handler.clj
+++ src/clj/dashboard/handler.clj
@@ -1,59 +1,56 @@
 (ns dashboard.handler
   (:gen-class)
   (:require
-    [compojure.core :refer :all]
-    [compojure.route :as route]
-    [compojure.middleware :refer [remove-trailing-slash]]
-    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-    [ring.middleware.gzip :refer [wrap-gzip]]
-    [ring.util.response :as response]
-    [ring.adapter.jetty :as jetty]
-    [dashboard.commands-container :as commands-container]
-    [dashboard.command-dispatcher :as dispatcher]))
+	[compojure.core :refer :all]
+	[compojure.route :as route]
+	[compojure.middleware :refer [remove-trailing-slash]]
+	[ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+	[ring.middleware.gzip :refer [wrap-gzip]]
+	[ring.util.response :as response]
+	[ring.adapter.jetty :as jetty]
+	[dashboard.commands-container :as commands-container]
+	[dashboard.command-dispatcher :as dispatcher]))
 
 
 (defn resource-response-as-html [resource]
   (response/content-type
-    (response/resource-response resource {:root "public"})
-    "text/html"))
+	(response/resource-response resource {:root "public"})
+	"text/html"))
 
-
+(defn response-as-json [body]
+  (-> body
+	(response/response)
+	(response/content-type "application/json")))
 
 (defroutes app-routes
   (context "/dashboard" []
-    (GET "/" [] (resource-response-as-html "index.html"))
-    (GET "/run/:section/:command" [section command] (dispatcher/dispatch {:command-type :dashboard-image
-                                                                          :section      section
-                                                                          :command      command}))
-    (GET "/commands" [] (-> :dashboard-image
-                          (commands-container/prepare-commands-from-coll)
-                          (response/response)
-                          (response/content-type "application/json"))) ; return dashboard commands
-    (route/resources "/")
-    (route/not-found "Page not found."))
+	(GET "/" [] (resource-response-as-html "index.html"))
+	(GET "/run/:section/:command" [section command] (response-as-json (dispatcher/dispatch {:command-type :dashboard
+																							:section      section
+																							:command      command})))
+	(GET "/commands" [] (response-as-json (commands-container/prepare-commands-from-coll :dashboard)))
+	(route/resources "/")
+	(route/not-found "Page not found."))
 
   (context "/advanced" []
-    (GET "/" [] (resource-response-as-html "advanced.html"))
-    (GET "/run/:section/:command" [section command] (dispatcher/dispatch {:command-type :advanced-image
-                                                                          :section      section
-                                                                          :command      command}))
-    (GET "/commands" [] (-> :advanced-image
-                          (commands-container/prepare-commands-from-coll)
-                          (response/response)
-                          (response/content-type "application/json"))) ; return advanced commands
-    (route/not-found "File not found."))
+	(GET "/" [] (resource-response-as-html "advanced.html"))
+	(GET "/run/:section/:command" [section command] (response-as-json (dispatcher/dispatch {:command-type :advanced
+																							:section      section
+																							:command      command})))
+	(GET "/commands" [] (response-as-json (commands-container/prepare-commands-from-coll :advanced)))
+	(route/not-found "File not found."))
 
   (context "/*" []
-    (GET "/" [] (response/redirect "/dashboard"))))
+	(GET "/" [] (response/redirect "/dashboard"))))
 
 (def app (-> (wrap-defaults app-routes site-defaults)
-           (wrap-gzip)))
+		   (wrap-gzip)))
 
 (defn -main [& args]
-	(let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
-		(jetty/run-jetty app {:port port
-                              :join? false
-                              :ssl? true
-                              :ssl-port 8443
-                              :keystore "resources/conf/keystore"
-                              :key-password "u1t4i8l4ity"})))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+	(jetty/run-jetty app {:port         port
+						  :join?        false
+						  :ssl?         true
+						  :ssl-port     8443
+						  :keystore     "resources/conf/keystore"
+						  :key-password "u1t4i8l4ity"})))
