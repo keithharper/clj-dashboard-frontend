50e33c4d5ca30a7574f79453b591304404747d0f
kharper <keithharper1155@gmail.com>
2017-12-22 09:14:19 -0500

    Made code more readable by using destructuring and shifting logic out of dispatch function into other functions.

--- src/clj/dashboard/dispatcher.clj
+++ src/clj/dashboard/dispatcher.clj
@@ -6,18 +6,22 @@
 	[clojure.data.json :as json]
 	[dashboard.shell-executor :as shell]))
 
-(defn prepare-command-execution-response [section command response]
-  (json/write-str {:section      section
-				   :command      command
-				   :execution-ts (.format (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss") (java.util.Date.))
-				   :result       response}))
+(defn now []
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss") (java.util.Date.)))
 
-(defn dispatch [{:keys [section command command-type] :as request}]
-  (try (let [command-arguments        (commands-container/filter-for-command-value request)
-			 command-execution-result (executor/run-command shell/run-in-bash command-arguments)
-			 command-result           (or (not-empty (:out command-execution-result)) ("fail"))]
-		 (prepare-command-execution-response section command command-result))
-	   (catch Exception e (prepare-command-execution-response section command "fail"))))
+(defn prepare-command-execution-response [{:keys [section command result] :as response}]
+  (json/write-str (assoc response :execution-ts (now))))
+
+(defn check-execution-result
+  [result]
+  (get result :out "fail"))
+
+(defn dispatch [request]
+  (try
+	(let [command-arguments (commands-container/filter-for-command-value request)
+		  execution-result  (check-execution-result (executor/run-command shell/run-in-bash command-arguments))]
+	  (prepare-command-execution-response (assoc request :result execution-result)))
+	(catch Exception e (prepare-command-execution-response (assoc request :result "fail")))))
 
 (defn dispatch-authentication [credentials]
   (authenticator/authenticate credentials))
